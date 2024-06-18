d6907c5c5320dab1fda31079acd00a5623653b0c
matinsekhavat <matinhyphen@gmail.com>
2024-06-18 15:00:02 +0330

    work with useForm (reavt-hook-form)

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,5 +1,38 @@
+import { useForm } from "react-hook-form";
 function Users() {
-  return <div>Users</div>;
+  //   const [input, setInput] = useState<string>("");
+  const {
+    getValues,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<{ email: string }>();
+  function handleEmail(formData) {
+    console.log(formData);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleEmail)}>
+        <input
+          {...register("email", {
+            required: {
+              message: "email is required",
+              value: true,
+            },
+            maxLength: {
+              message: "Too many characters",
+              value: 8,
+            },
+          })}
+          placeholder="email"
+        />
+      </form>
+      {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+    </div>
+  );
 }
 
 export default Users;
+
+// instead of passing onChange onBlur ref ... we pass {...register}
